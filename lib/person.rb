761befd1634165eba2a6759fb75a01a2b5f8f998
JustinMitchell <mitchelljm04@gmail.com>
2022-10-14 10:28:50 -0400

    commit tree 263a80bb95b32bb860f368c0d9e706c23969d76c

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  # your code here
+
+  def initialize(arguments)
+    arguments.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
+
 end
+
+bob_attributes = {name: "bob", hair_color: "Brown"}
+
+bob = Person.new(bob_attributes)
